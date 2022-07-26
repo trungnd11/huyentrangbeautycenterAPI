7de476235b151429494ce99ab494bef5687a6302
trungnd11 <trungnd1@devcamp.edu.vn>
2022-07-26 16:55:21 +0700

    Update API mesenger

--- controllers/About.controller.js
+++ controllers/About.controller.js
@@ -2,7 +2,7 @@ import { AboutModel } from "../models/About.model.js";
 
 export const getAbout = async (req, res) => {
   try {
-    const about = await AboutModel.find();
+    const about = await AboutModel.findOne({}, {}, { sort: { createdAt: -1 } });
     return res.status(200).json(about);
   } catch (error) {
     return res.status(500).json({ error });
