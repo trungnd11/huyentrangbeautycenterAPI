f332e293cd6f7eca94bfb37b03ce1b0607cc88bf
trungnd11 <trungnd1@devcamp.edu.vn>
2022-07-30 08:51:53 +0700

    Update banner

--- models/Banner.model.js
+++ models/Banner.model.js
@@ -6,7 +6,9 @@ const BannerSchema = new Schema({
   img: {
     type: String,
     required: true
-  }
+  },
+  title: String,
+  content: String,
 }, { timestamps: true });
 
 export const BannerModel = mongoose.model("banner", BannerSchema);
--- routers/Banner.router.js
+++ routers/Banner.router.js
@@ -1,9 +1,10 @@
 import experss from "express";
-import { createBanner, getBanner } from "../controllers/Banner.controller.js";
+import { createBanner, getBanner, updateBanner } from "../controllers/Banner.controller.js";
 
 const banner = experss.Router();
 
 banner.get("/banners", getBanner);
 banner.post("/banners", createBanner);
+banner.put("/banners", updateBanner);
 
 export default banner;
