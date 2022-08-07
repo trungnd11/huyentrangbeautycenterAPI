b0004f80687d8a8e4c9bad7d475315184049a156
trungnd9 <96941235+trungnd9@users.noreply.github.com>
2022-08-07 09:52:20 +0700

    Update Banner API

--- controllers/Banner.controller.js
+++ controllers/Banner.controller.js
@@ -2,7 +2,7 @@ import { BannerModel } from "../models/Banner.model.js";
 
 export const getBanner = async (req, res) => {
   try {
-    const banners = await BannerModel.find()
+    const banners = await BannerModel.find().sort({ updatedAt: -1 });
     return res.status(200).json(banners);
   } catch (error) {
     return res.status(500).json({ error });
--- models/Banner.model.js
+++ models/Banner.model.js
@@ -9,6 +9,7 @@ const BannerSchema = new Schema({
   },
   title: String,
   content: String,
+  nameImg: String
 }, { timestamps: true });
 
 export const BannerModel = mongoose.model("banner", BannerSchema);
